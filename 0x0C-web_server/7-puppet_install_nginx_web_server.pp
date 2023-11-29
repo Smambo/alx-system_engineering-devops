09bf9dadc6522bae8b75ee00677e770d95beccf6
Simamnkele <smambo85@gmail.com>
2023-11-29 19:41:42 +0200

    Added puppet script that configures server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+#installs and configures nginx server
+exec {'update system':
+  command => 'sudo apt-get -y update',
+}
+
+package {'nginx':
+  ensure  => 'installed',
+  require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
