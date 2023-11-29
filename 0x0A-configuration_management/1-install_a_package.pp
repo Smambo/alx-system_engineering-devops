ced2e222e811cd744b84e77653f7c1ade705e913
Simamnkele <smambo85@gmail.com>
2023-11-29 13:26:00 +0200

    Refactored script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,10 +6,10 @@ package {'python3-pip':
 }
 
 exec {'install flask':
-  command => 'pip3 install flask',
-  ensure  => '2.1.0'
-  unless  => 'pip3 show flask',
-  path    => ['/usr/local/bin/', '/usr/bin/', '/bin'],
-  user    => 'root',
+  command     => 'pip3 install "flask==2.1.0"',
+  unless      => 'pip3 show flask | grep "Version: 2.1.0"',
+  path        => ['/usr/local/bin/', '/usr/bin/', '/bin'],
+  user        => root,
+  environment => ['HOME=/root'],
 }
 
