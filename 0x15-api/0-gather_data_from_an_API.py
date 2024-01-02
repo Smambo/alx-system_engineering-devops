523a3983dfb22a79842d8e27e1b2dcf6a4c5ce24
Simamnkele <smambo85@gmail.com>
2024-01-02 20:03:08 +0200

    Pushed api data gathering script

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Import modules for api script"""
+from sys import argv
+import requests
+
+
+def todo_api(userid):
+    """Returns information about employees TODO list progress."""
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        userid)).json().get('name')
+    task = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+                userid)).json()
+    tasks_done = ['\t {}\n'.format(dic.get('title')) for dic in task
+                  if dic.get('completed')]
+    if name and task:
+        print("Employee {} is done with tasks({}/{}):".format(
+            name, len(tasks_done), len(task)))
+        print(''.join(tasks_done), end='')
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        todo_api(int(argv[1]))
