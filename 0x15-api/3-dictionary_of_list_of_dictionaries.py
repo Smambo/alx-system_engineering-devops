4bba4a9bf6f7795f41615167047639951c8feeab
Simamnkele <smambo85@gmail.com>
2024-01-02 21:19:57 +0200

    Pushed dict json script

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Import modules for json script."""
+import json
+from requests import get
+from sys import argv
+
+
+def json_export():
+    """Exports data in JSON format."""
+    data = get('https://jsonplaceholder.typicode.com/users').json()
+    ids = [(dic.get('id'), dic.get('username')) for dic in data]
+
+    dumps = {}
+
+    for user in ids:
+        data = get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                    user[0])).json()
+        records = [{"task": line.get('title'), "completed":
+                    line.get('completed'), "username": user[1]}
+                    for line in data]
+        dumps[user[0]] = records
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(dumps, f)
+
+
+if __name__ == "__main__":
+    json_export()
