dbc9836d82ec2231b781326be81325e14e576869
Simamnkele <smambo85@gmail.com>
2024-01-17 20:58:08 +0200

    Pushed puppet script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# debugging apache 500 error
+exec { 'replaces wrong php filetype':
+  command => 'sed -i "s/ .phpp/ .php/g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+}
