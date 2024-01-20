254408558175237b002964cb86843ec5b63a288b
Simamnkele <smambo85@gmail.com>
2024-01-20 17:40:04 +0000

    Debugged server error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # debugging apache 500 error
 exec { 'replaces wrong php filetype':
-  command => 'sed -i "s/ .phpp/ .php/g" /var/www/html/wp-settings.php',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /var/www/html/wp-settings.php'
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
